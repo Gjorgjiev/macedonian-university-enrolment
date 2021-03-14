c3c5dfacbb949cc2e0fc94a4f5433a1518e95de1
Sam Ruby <dare-gjorgjiev@hotmail.com>
2021-03-14 23:17:36 +0100

    home page redesign

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'index/home'
-  
+  #get 'startup/index'
+  root to: 'startup#index'
+  get 'login', to:'pages#login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
